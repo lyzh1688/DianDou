06e4e5d408685f1d22108927175f867f79445aac
Jennifer <Jennifer>
2016-05-03 08:54:12 +0800

    raw page of the app front view

---
+++ app/diandou/www/js/app.js
@@ -0,0 +1,120 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+    $ionicConfigProvider.platform.ios.tabs.style('standard');
+    $ionicConfigProvider.platform.ios.tabs.position('bottom');
+    $ionicConfigProvider.platform.android.tabs.style('standard');
+    $ionicConfigProvider.platform.android.tabs.position('standard');
+
+    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+    $ionicConfigProvider.platform.ios.views.transition('ios');
+    $ionicConfigProvider.platform.android.views.transition('android');
+
+
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+  .state('app.main', {
+      url: '/main',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/main.html'
+        }
+      }
+    })
+    .state('app.main.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/home.html"
+        }
+      }
+    })
+    .state('app.main.home.interest', {
+      url: "/interest",
+      views: {
+        'home-sub': {
+          templateUrl: "templates/interest.html"
+        }
+      }
+    })
+    .state('app.main.home.recommend', {
+      url: "/recommend",
+      views: {
+        'home-sub': {
+          templateUrl: "templates/recommend.html"
+        }
+      }
+    })
+    .state('app.main.discover', {
+      url: "/discover",
+      views: {
+        'discover-tab': {
+          templateUrl: "templates/discover.html"
+        }
+      }
+    })
+    .state('app.main.discover.star', {
+      url: "/star",
+      views: {
+        'discover-sub': {
+          templateUrl: "templates/star.html"
+        }
+      }
+    })
+    .state('app.main.discover.lecturers', {
+      url: "/lecturers",
+      views: {
+        'discover-sub': {
+          templateUrl: "templates/lecturers.html"
+        }
+      }
+    })
+
+
+
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/main');
+});
